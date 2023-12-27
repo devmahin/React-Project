3d732eebacf1701ef4241d5cac60210785c7bb9b
devmahin <mahinhowlader123@gmail.com>
2023-12-27 20:17:58 +0600

    first commit

---
+++ project/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+import Demo from "./demo";
+
+const App = () => {
+  return (
+    <div>
+      <Demo/>
+    </div>
+  );
+};
+
+export default App;
---
+++ project/vite-project/src/Demo.jsx
@@ -0,0 +1,10 @@
+
+const Demo = () => {
+    return (
+        <div>
+            <h1>I am demo function compunent</h1>
+        </div>
+    );
+};
+
+export default Demo;
